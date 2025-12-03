613fbdff7c62a6509536a8acd0549cac6a633f4d
nlao1 <nlao@seas.upenn.edu>
2025-12-03 00:44:00 -0500

    day 3 fixups

--- 03/solution.py
+++ 03/solution.py
@@ -10,43 +10,41 @@ def parse(s: list[str]):
     return [line.strip() for line in s]
 
 
-def max_joltage_part_1(bank: list[int]):
-    max_start_battery: Optional[int] = None
-    curr_max_joltage: Optional[int] = None
-    for i, battery_joltage in enumerate(bank):
-        if i == len(bank) - 1:
-            break
-        if max_start_battery is None or battery_joltage > max_start_battery:
-            candidate = 10 * battery_joltage + max(bank[i + 1 :])
-            if curr_max_joltage is None or candidate > curr_max_joltage:
-                max_start_battery = battery_joltage
-                curr_max_joltage = candidate
-    return curr_max_joltage
-
-
-def max_joltage(bank: list[int], necessary_remaining):
-    # i think this has optimal substructure
-    # find the leftmost max number that leaves at least `necessary_remaining`,
-    # then recursively call on the slice after
-    if necessary_remaining == 1:
-        return max(bank)
-    curr_max = -1
-    curr_max_index = -1
-    for i in range(len(bank) - necessary_remaining, -1, -1):
-        joltage = bank[i]
-        if curr_max is None or joltage >= curr_max:
-            curr_max = joltage
-            curr_max_index = i
-    return curr_max * (10 ** (necessary_remaining - 1)) + max_joltage(
-        bank[curr_max_index + 1 :], necessary_remaining - 1
-    )
-
-
 def part1(banks: list[str]):
-    return sum(max_joltage_part_1([int(x) for x in bank]) for bank in banks)
+    def max_joltage(bank: list[int]):
+        max_start_battery: Optional[int] = None
+        curr_max_joltage: Optional[int] = None
+        for i, battery_joltage in enumerate(bank):
+            if i == len(bank) - 1:
+                break
+            if max_start_battery is None or battery_joltage > max_start_battery:
+                candidate = 10 * battery_joltage + max(bank[i + 1 :])
+                if curr_max_joltage is None or candidate > curr_max_joltage:
+                    max_start_battery = battery_joltage
+                    curr_max_joltage = candidate
+        return curr_max_joltage
+
+    return sum(max_joltage([int(x) for x in bank]) for bank in banks)
 
 
 def part2(banks: list[str]):
+    def max_joltage(bank: list[int], necessary_remaining):
+        # i think this has optimal substructure
+        # find the leftmost max number that leaves at least `necessary_remaining`,
+        # then recursively call on the slice after
+        if necessary_remaining == 1:
+            return max(bank)
+        curr_max = -1
+        curr_max_index = -1
+        for i in range(len(bank) - necessary_remaining, -1, -1):
+            joltage = bank[i]
+            if curr_max is None or joltage >= curr_max:
+                curr_max = joltage
+                curr_max_index = i
+        return curr_max * (10 ** (necessary_remaining - 1)) + max_joltage(
+            bank[curr_max_index + 1 :], necessary_remaining - 1
+        )
+
     return sum(max_joltage([int(x) for x in bank], 12) for bank in banks)
 
 
